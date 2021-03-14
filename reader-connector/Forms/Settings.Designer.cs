bc0ca3b8ea74ea0991bc5ac7c3e95b3405670c92
cqhung1412 <42210002+cqhung1412@users.noreply.github.com>
2021-03-14 15:36:05 +0700

    test add txtbox

--- reader-connector/Forms/Settings.Designer.cs
+++ reader-connector/Forms/Settings.Designer.cs
@@ -29,12 +29,41 @@ namespace reader_connector.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TxtTcp = new System.Windows.Forms.TextBox();
+            this.TxtCourseId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TxtTcp
+            // 
+            this.TxtTcp.Location = new System.Drawing.Point(42, 12);
+            this.TxtTcp.Name = "TxtTcp";
+            this.TxtTcp.Size = new System.Drawing.Size(100, 20);
+            this.TxtTcp.TabIndex = 0;
+            // 
+            // TxtCourseId
+            // 
+            this.TxtCourseId.Location = new System.Drawing.Point(42, 39);
+            this.TxtCourseId.Name = "TxtCourseId";
+            this.TxtCourseId.Size = new System.Drawing.Size(100, 20);
+            this.TxtCourseId.TabIndex = 1;
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtCourseId);
+            this.Controls.Add(this.TxtTcp);
+            this.Name = "Settings";
             this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TxtTcp;
+        private System.Windows.Forms.TextBox TxtCourseId;
     }
 }
