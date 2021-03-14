153e51d1b8f6ad1969cbc316d46ce0d75b17b29a
cqhung1412 <42210002+cqhung1412@users.noreply.github.com>
2021-03-14 22:03:10 +0700

    Change font

--- reader-connector/Forms/Settings.Designer.cs
+++ reader-connector/Forms/Settings.Designer.cs
@@ -32,23 +32,23 @@ namespace reader_connector.Forms
             this.components = new System.ComponentModel.Container();
             this.TxtCourseId = new System.Windows.Forms.TextBox();
             this.GrpSetup = new System.Windows.Forms.GroupBox();
+            this.ComboTcp = new System.Windows.Forms.ComboBox();
             this.BtnConnect = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.label3 = new System.Windows.Forms.Label();
             this.GrpSettings = new System.Windows.Forms.GroupBox();
+            this.BtnGetSettings = new System.Windows.Forms.Button();
             this.BtnSetSettings = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.TxtTagFilter = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.TxtAutosleep = new System.Windows.Forms.TextBox();
-            this.BtnGetSettings = new System.Windows.Forms.Button();
             this.TxtLog = new System.Windows.Forms.RichTextBox();
             this.BtnStart = new System.Windows.Forms.Button();
             this.BtnClearLog = new System.Windows.Forms.Button();
             this.BtnExit = new System.Windows.Forms.Button();
-            this.ComboTcp = new System.Windows.Forms.ComboBox();
             this.GrpSetup.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.GrpSettings.SuspendLayout();
@@ -56,9 +56,10 @@ namespace reader_connector.Forms
             // 
             // TxtCourseId
             // 
+            this.TxtCourseId.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtCourseId.Location = new System.Drawing.Point(79, 45);
             this.TxtCourseId.Name = "TxtCourseId";
-            this.TxtCourseId.Size = new System.Drawing.Size(154, 20);
+            this.TxtCourseId.Size = new System.Drawing.Size(154, 21);
             this.TxtCourseId.TabIndex = 1;
             // 
             // GrpSetup
@@ -68,12 +69,22 @@ namespace reader_connector.Forms
             this.GrpSetup.Controls.Add(this.label2);
             this.GrpSetup.Controls.Add(this.label1);
             this.GrpSetup.Controls.Add(this.TxtCourseId);
+            this.GrpSetup.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.GrpSetup.Location = new System.Drawing.Point(12, 12);
             this.GrpSetup.Name = "GrpSetup";
             this.GrpSetup.Size = new System.Drawing.Size(322, 74);
             this.GrpSetup.TabIndex = 2;
             this.GrpSetup.TabStop = false;
             this.GrpSetup.Text = "Setup";
+            // 
+            // ComboTcp
+            // 
+            this.ComboTcp.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboTcp.FormattingEnabled = true;
+            this.ComboTcp.Location = new System.Drawing.Point(79, 19);
+            this.ComboTcp.Name = "ComboTcp";
+            this.ComboTcp.Size = new System.Drawing.Size(154, 21);
+            this.ComboTcp.TabIndex = 6;
             // 
             // BtnConnect
             // 
@@ -88,27 +99,28 @@ namespace reader_connector.Forms
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 48);
+            this.label2.Location = new System.Drawing.Point(7, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.Size = new System.Drawing.Size(60, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "Course ID";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Location = new System.Drawing.Point(6, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.Size = new System.Drawing.Size(74, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "TCP Address";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(19, 89);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.Size = new System.Drawing.Size(25, 15);
             this.label3.TabIndex = 4;
             this.label3.Text = "Log";
             // 
@@ -120,12 +132,23 @@ namespace reader_connector.Forms
             this.GrpSettings.Controls.Add(this.TxtTagFilter);
             this.GrpSettings.Controls.Add(this.label5);
             this.GrpSettings.Controls.Add(this.TxtAutosleep);
+            this.GrpSettings.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.GrpSettings.Location = new System.Drawing.Point(340, 12);
             this.GrpSettings.Name = "GrpSettings";
             this.GrpSettings.Size = new System.Drawing.Size(265, 74);
             this.GrpSettings.TabIndex = 6;
             this.GrpSettings.TabStop = false;
             this.GrpSettings.Text = "Settings";
+            // 
+            // BtnGetSettings
+            // 
+            this.BtnGetSettings.Location = new System.Drawing.Point(184, 17);
+            this.BtnGetSettings.Name = "BtnGetSettings";
+            this.BtnGetSettings.Size = new System.Drawing.Size(75, 23);
+            this.BtnGetSettings.TabIndex = 6;
+            this.BtnGetSettings.Text = "Get";
+            this.BtnGetSettings.UseVisualStyleBackColor = true;
+            this.BtnGetSettings.Click += new System.EventHandler(this.BtnGetSettings_Click);
             // 
             // BtnSetSettings
             // 
@@ -140,50 +163,42 @@ namespace reader_connector.Forms
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 48);
+            this.label4.Location = new System.Drawing.Point(12, 48);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.Size = new System.Drawing.Size(61, 15);
             this.label4.TabIndex = 4;
             this.label4.Text = "Autosleep";
             // 
             // TxtTagFilter
             // 
+            this.TxtTagFilter.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtTagFilter.Location = new System.Drawing.Point(79, 19);
             this.TxtTagFilter.Name = "TxtTagFilter";
-            this.TxtTagFilter.Size = new System.Drawing.Size(100, 20);
+            this.TxtTagFilter.Size = new System.Drawing.Size(100, 21);
             this.TxtTagFilter.TabIndex = 0;
             this.TxtTagFilter.Text = "6000";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 22);
+            this.label5.Location = new System.Drawing.Point(16, 22);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.Size = new System.Drawing.Size(57, 15);
             this.label5.TabIndex = 3;
             this.label5.Text = "Tag Filter";
             // 
             // TxtAutosleep
             // 
+            this.TxtAutosleep.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtAutosleep.Location = new System.Drawing.Point(79, 45);
             this.TxtAutosleep.Name = "TxtAutosleep";
-            this.TxtAutosleep.Size = new System.Drawing.Size(100, 20);
+            this.TxtAutosleep.Size = new System.Drawing.Size(100, 21);
             this.TxtAutosleep.TabIndex = 1;
             this.TxtAutosleep.Text = "50";
             // 
-            // BtnGetSettings
-            // 
-            this.BtnGetSettings.Location = new System.Drawing.Point(184, 17);
-            this.BtnGetSettings.Name = "BtnGetSettings";
-            this.BtnGetSettings.Size = new System.Drawing.Size(75, 23);
-            this.BtnGetSettings.TabIndex = 6;
-            this.BtnGetSettings.Text = "Get";
-            this.BtnGetSettings.UseVisualStyleBackColor = true;
-            this.BtnGetSettings.Click += new System.EventHandler(this.BtnGetSettings_Click);
-            // 
             // TxtLog
             // 
-            this.TxtLog.Font = new System.Drawing.Font("Lucida Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtLog.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtLog.Location = new System.Drawing.Point(12, 106);
             this.TxtLog.Name = "TxtLog";
             this.TxtLog.Size = new System.Drawing.Size(506, 453);
@@ -192,6 +207,7 @@ namespace reader_connector.Forms
             // 
             // BtnStart
             // 
+            this.BtnStart.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnStart.Location = new System.Drawing.Point(524, 106);
             this.BtnStart.Name = "BtnStart";
             this.BtnStart.Size = new System.Drawing.Size(75, 23);
@@ -202,6 +218,7 @@ namespace reader_connector.Forms
             // 
             // BtnClearLog
             // 
+            this.BtnClearLog.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnClearLog.Location = new System.Drawing.Point(524, 135);
             this.BtnClearLog.Name = "BtnClearLog";
             this.BtnClearLog.Size = new System.Drawing.Size(75, 23);
@@ -212,6 +229,7 @@ namespace reader_connector.Forms
             // 
             // BtnExit
             // 
+            this.BtnExit.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnExit.Location = new System.Drawing.Point(524, 536);
             this.BtnExit.Name = "BtnExit";
             this.BtnExit.Size = new System.Drawing.Size(75, 23);
@@ -219,14 +237,6 @@ namespace reader_connector.Forms
             this.BtnExit.Text = "Exit";
             this.BtnExit.UseVisualStyleBackColor = true;
             this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
-            // 
-            // ComboTcp
-            // 
-            this.ComboTcp.FormattingEnabled = true;
-            this.ComboTcp.Location = new System.Drawing.Point(79, 19);
-            this.ComboTcp.Name = "ComboTcp";
-            this.ComboTcp.Size = new System.Drawing.Size(154, 21);
-            this.ComboTcp.TabIndex = 6;
             // 
             // Settings
             // 
